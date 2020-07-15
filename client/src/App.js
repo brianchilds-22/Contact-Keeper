6c2f9c0ee117d4afd6db53d3fb9214f2014f116e
brianchilds-22 <brianchilds22@msn.com>
2020-07-14 19:24:58 -0600

    Auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
 
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 import './App.css';
 
 
 const App = () => {
   return (
+    <AuthState>
     <ContactState>
       <Router> 
         <Fragment>
@@ -18,11 +21,13 @@ const App = () => {
             <Switch>
               <Route exact path='/' component={Home} />
               <Route exact path='/about' component={About} />
+              <Route exact path='/register' component={Register} />
             </Switch>
           </div>
         </Fragment>
       </Router>
     </ContactState>
+    </AuthState>
   );
 };
 
